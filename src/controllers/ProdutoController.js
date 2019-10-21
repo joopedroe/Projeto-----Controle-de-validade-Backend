c4b918e0fa0f21715f5c556f3185413ace178d08
João Pedro <joopedroe936@gmail.com>
2019-10-20 21:06:06 -0300

    Ordenar data

--- src/controllers/ProdutoController.js
+++ src/controllers/ProdutoController.js
@@ -36,7 +36,7 @@ module.exports =
     },
     
     async index(req, res){
-        const listaProduto = await Produto.find({status:true}).sort({data: 'desc'});
+        const listaProduto = await Produto.find({status:true}).sort({data_validade: 'desc'});
         return res.json(listaProduto);
 
     },
