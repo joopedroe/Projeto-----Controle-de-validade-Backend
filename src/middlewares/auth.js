efbe4376e55b42bca3fc29230888cdc586e10d4f
João Pedro <joopedroe936@gmail.com>
2019-12-06 21:29:21 -0300

    habilitar token

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -22,6 +22,6 @@ module.exports=(req,res,next)=>{
         if(err) return res.status(401).send({error:'Token invalido'});
 
         req.userId = decoded.id;
-        return next();
+        return res.status(200).next();
     })
 }
