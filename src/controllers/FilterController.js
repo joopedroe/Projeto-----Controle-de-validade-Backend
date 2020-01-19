1dcbe0a974bbb845a8646f30c2304735f4819fc4
João Pedro <joopedroe936@gmail.com>
2020-01-18 22:31:19 -0300

    adicionar filtros

--- src/controllers/FilterController.js
+++ src/controllers/FilterController.js
@@ -3,8 +3,15 @@ const Produto = require('../models/Produto');
 module.exports ={
     async indexFilter(req,res){
         const data=req.headers.data_validadeentrada
-        const filterProduto = await Produto.find({status:true,data_validade:{$lte:data}}).sort({data_validade: 'asc'});
-        console.log(data)
-        return res.json(filterProduto);
+        const secao=req.headers.secao
+        var hora=" 08:00:00"
+        if (secao !== "Todos"){
+            const filterProduto = await Produto.find({status:true,data_validade:{$lte:data+hora},valor:secao}).sort({data_validade: 'asc'});
+            return res.json(filterProduto);
+        }
+        else{
+            const filterProduto = await Produto.find({status:true,data_validade:{$lte:data+hora}}).sort({data_validade: 'asc'});
+            return res.json(filterProduto);
+        }
     }
 }
